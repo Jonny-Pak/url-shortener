cfa413dfd9492386ad7fb6a31894d8cc13375ce6
Ngọc Thuận <thuanln1907@gmail.com>
2025-10-24 19:41:37 +0700

    Tuan1
    Hoàn Thành

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -2,19 +2,18 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
-import { jwtConstants } from '../constants';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private config: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwtConstants.secret,
+      secretOrKey: config.get<string>('JWT_SECRET'),
     });
   }
-
-async validate(payload: any) {
-  return { userId: payload.sub, email: payload.email, roles: payload.roles };
-}
+  async validate(payload: any) {
+    return { userId: payload.sub, email: payload.email, roles: payload.roles };
+  }
 }
